68423cb3a8dc06d8fcf44b9edaf923475c4c1b99
WassCodeur <wachioubouraima56@gmail.com>
2023-11-29 15:37:23 +0000

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ import re
 from typing import List
 
 
-PII_FIELDS = ("name", "phone", "ssn", "password", "ip")
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
